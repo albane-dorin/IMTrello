f11c20b06da9c05e4e7e620f466bd16fc97a73e4
Albane <albane.dorin@gmail.com>
2024-02-22 20:58:22 +0100

    Ajout méthode connect et create-account pour user

--- TrelloClass.py
+++ TrelloClass.py
@@ -9,31 +9,15 @@ class Trello:
     def get_users(self):
         return self.users
 
-    def create_account(self):
-
+    def create_account(self, name, password, role):
         users = getattr(self, "users")
-        name = input("Enter your username: ")
-        while True:
-            password = input("Enter your password: ")
-            confirm = input("Enter the same password: ")  #Check the password
-            if password == confirm:
-                break
-            else:
-                print("The two password aren't the same")
-        while True:
-            role = input("To be Developer enter 0, to be Manager enter 1, to be Both enter 2: ")
-            match role:
-                case "0":
-                    users.append(Developer(name, password))
-                    break
-                case "1":
-                    users.append(Manager(name, password))
-                    break
-                case "2":
-                    users.append(ManDev(name, password))
-                    break
-                case _:
-                    print("Invalid entry")
+        match role:
+            case "0":
+                users.append(Developer(name, password))
+            case "1":
+                users.append(Manager(name, password))
+            case "2":
+                users.append(ManDev(name, password))
 
     def can_connect_user(self, name, password):
         exist = False
--- userClass.py
+++ userClass.py
@@ -1,15 +1,56 @@
+import TrelloClass
 
-class User :
+
+class User:
     last_id = 0
 
     def __init__(self, name, password):
         User.last_id = User.last_id + 1
         self.id = User.last_id
-        self.id=id
+        self.id = id
         self.name = name
         self.password = password
         self.isDeveloper = None
         self.isManager = None
+        
+        def get_name(self):
+            return self.name
+        
+        def get_id(self):
+            return self.id
+
+        def get_password(self):
+            return self.password
+
+        def connect(self, trello):
+            while True:
+                name = input("Enter your username: ")
+                password = input("Enter your password: ")
+                if trello.can_connect_user(name, password):
+                    break
+                else:
+                    print("Nom d'utilisateur ou mot de passe incorrect, réessayez ou créez un compte")
+                    choix = input("1 pour réessayer, 2 pour créer un compte")
+                    if choix == "2": break
+
+        def create_account(self, trello):
+            name = input("Enter your username: ")
+            while True:
+                password = input("Enter your password: ")
+                confirm = input("Enter the same password: ")  # Check the password
+                if password == confirm:
+                    break
+                else:
+                    print("The two password aren't the same")
+            while True:
+                role = input("To be Developer enter 0, to be Manager enter 1, to be Both enter 2: ")
+                match role:
+                    case "0" | "1" | "2":
+                        trello.create_account(name, password, role)
+                        break
+                    case _:
+                        print("Invalid entry")
+
 
 class Developer(User):
     def __init__(self, name, password):
